c666c742e3cf2847d0564d8ffb2d9f531ff0e490
quangdn <quangdn@gemvietnam.com>
2024-10-06 23:13:52 +0700

    jump game

---
+++ src/main/java/com/example/demo/algorithm/JumpGame.java
@@ -0,0 +1,27 @@
+package com.example.demo.algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/jump-game/description/
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int targetIdx = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] + i < targetIdx) {
+                targetIdx = i;
+            }
+            if (targetIdx == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{2, 3, 1, 1, 4};
+        JumpGame s = new JumpGame();
+        s.canJump(a);
+    }
+}
